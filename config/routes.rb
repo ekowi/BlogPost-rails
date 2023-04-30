af00020e1722f36ba165965577ca78398e08ad6a
ekowi <ekowi02207@gmail.com>
2023-04-30 14:55:05 +0700

    First Commit for our Blog Post

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # resources :blog_post
+
+  get "/blog_posts/new", to: "blog_post#new", as: :new_blog_post
+  get "/blog_posts/:id", to: "blog_post#show", as: :blog_post
+  patch "/blog_posts/:id", to: "blog_post#update"
+  delete "/blog_posts/:id", to: "blog_post#destroy"
+  get "/blog_posts/:id/edit", to: "blog_post#edit", as: :edit_blog_post
+  post "/blog_posts", to: "blog_post#create", as: :blog_posts
+
+
+  # Defines the root path route ("/")
+  root "blog_post#index"
+end
